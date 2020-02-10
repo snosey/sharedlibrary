e165ad868528e1abf9e3427273abaf1cdfb6192a
Snosey-Mac <ahmed.el.snosey@gmail.com>
2020-02-10 17:46:35 +0200

    update jsonConverter to return list

--- app/src/main/java/com/dinnova/sharedlibrary/webservice/JsonConverter.java
+++ app/src/main/java/com/dinnova/sharedlibrary/webservice/JsonConverter.java
@@ -1,11 +1,16 @@
 package com.dinnova.sharedlibrary.webservice;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class JsonConverter implements Serializable {
     public JSONObject modelToJson() throws JSONException {
@@ -16,8 +21,8 @@ public class JsonConverter implements Serializable {
         return new Gson().fromJson(new JSONObject(jsonObject).getJSONObject(WebService.Data).toString(), super.getClass());
     }
 
-    public Object jsonToListModel(String jsonObject) throws JSONException {
-        return new Gson().fromJson(new JSONObject(jsonObject).getJSONArray(WebService.Data).toString(), super.getClass());
+    public Object[] jsonToListModel(String jsonObject) throws JSONException {
+        Object [] array = (Object[])java.lang.reflect.Array.newInstance(super.getClass(), 0);
+        return new Gson().fromJson(new JSONObject(jsonObject).getJSONArray(WebService.Data).toString(),array.getClass());
     }
-
 }
