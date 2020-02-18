539b22786dd1f72a24ceddf8ad45f551b50a3933
Snosey-Mac <ahmed.el.snosey@gmail.com>
2020-02-18 04:03:09 +0200

    fix exclude and include params

--- app/src/main/java/com/dinnova/sharedlibrary/webservice/JsonConverter.java
+++ app/src/main/java/com/dinnova/sharedlibrary/webservice/JsonConverter.java
@@ -10,19 +10,25 @@ import java.io.Serializable;
 import java.util.Arrays;
 
 public class JsonConverter implements Serializable {
-    Gson gson = new GsonBuilder()
-            .excludeFieldsWithoutExposeAnnotation()
-            .create();
 
     public JSONObject modelToJson() throws JSONException {
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create();
         return new JSONObject(gson.toJson(this));
     }
 
     public Object jsonToModel(String jsonObject) throws JSONException {
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create();
         return gson.fromJson(new JSONObject(jsonObject).getJSONObject(WebService.Data).toString(), super.getClass());
     }
 
     public Object jsonToListModel(String jsonObject) throws JSONException {
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create();
         Object[] array = (Object[]) java.lang.reflect.Array.newInstance(super.getClass(), 0);
         return Arrays.asList(gson.fromJson(new JSONObject(jsonObject).getJSONArray(WebService.Data).toString(), array.getClass()));
     }
