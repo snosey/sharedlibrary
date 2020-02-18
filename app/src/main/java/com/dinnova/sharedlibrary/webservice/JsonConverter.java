5c7b79dc2c663eae3f9fb46bf48f6e33a6915c23
Snosey-Mac <ahmed.el.snosey@gmail.com>
2020-02-18 03:43:21 +0200

    exclude and include params

--- app/src/main/java/com/dinnova/sharedlibrary/webservice/JsonConverter.java
+++ app/src/main/java/com/dinnova/sharedlibrary/webservice/JsonConverter.java
@@ -1,29 +1,29 @@
 package com.dinnova.sharedlibrary.webservice;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.GsonBuilder;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 
 public class JsonConverter implements Serializable {
+    Gson gson = new GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create();
+
     public JSONObject modelToJson() throws JSONException {
-        return new JSONObject(new Gson().toJson(this));
+        return new JSONObject(gson.toJson(this));
     }
 
     public Object jsonToModel(String jsonObject) throws JSONException {
-        return new Gson().fromJson(new JSONObject(jsonObject).getJSONObject(WebService.Data).toString(), super.getClass());
+        return gson.fromJson(new JSONObject(jsonObject).getJSONObject(WebService.Data).toString(), super.getClass());
     }
 
     public Object jsonToListModel(String jsonObject) throws JSONException {
-        Object [] array = (Object[])java.lang.reflect.Array.newInstance(super.getClass(), 0);
-        return Arrays.asList( new Gson().fromJson(new JSONObject(jsonObject).getJSONArray(WebService.Data).toString(),array.getClass()));
+        Object[] array = (Object[]) java.lang.reflect.Array.newInstance(super.getClass(), 0);
+        return Arrays.asList(gson.fromJson(new JSONObject(jsonObject).getJSONArray(WebService.Data).toString(), array.getClass()));
     }
 }
