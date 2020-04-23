de2a11c81edd3fb7bb0c5e2cb561d3501be6019c
Snosey-Mac <ahmed.el.snosey@gmail.com>
2020-04-23 06:41:29 +0200

    update

--- app/src/main/java/com/dinnova/sharedlibrary/notification/NotificationReceiving.java
+++ app/src/main/java/com/dinnova/sharedlibrary/notification/NotificationReceiving.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.util.Log;
 
 import com.dinnova.sharedlibrary.utils.views.CustomApplication;
+import com.dinnova.sharedlibrary.webservice.WebService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import org.json.JSONObject;
@@ -21,14 +22,16 @@ public class NotificationReceiving extends com.google.firebase.messaging.Firebas
     private void notificationRecieved(RemoteMessage remoteMessage, NotificationListener notificationListener) {
         Log.e("NotificationId", remoteMessage.getMessageId() + "");
         try {
-            NotificationModel notificationModel = (NotificationModel) new NotificationModel().jsonToModel(new JSONObject(remoteMessage.getData()).toString());
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put(WebService.Data, new JSONObject(remoteMessage.getData()));
+            NotificationModel notificationModel = (NotificationModel) new NotificationModel().jsonToModel(jsonObject.toString());
             notificationModel.convertData();
             NotificationChannel mChannel = null;
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                mChannel = new NotificationChannel(notificationModel.NotificationTypeModel.Id+"", notificationModel.NotificationTypeModel.Name, NotificationManager.IMPORTANCE_HIGH);
+                mChannel = new NotificationChannel(notificationModel.NotificationTypeModel.Id + "", notificationModel.NotificationTypeModel.Name, NotificationManager.IMPORTANCE_HIGH);
             }
-            NotificationDisplay notificationDisplay=new NotificationDisplay(this,notificationModel,mChannel,notificationListener);
-            notificationListener.notificationReceived(notificationModel,notificationDisplay);
+            NotificationDisplay notificationDisplay = new NotificationDisplay(this, notificationModel, mChannel, notificationListener);
+            notificationListener.notificationReceived(notificationModel, notificationDisplay);
 
         } catch (Exception e) {
             e.printStackTrace();
