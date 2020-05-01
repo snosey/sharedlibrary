6f3a1dd36248fd128eeacc60dca6fe604bed9718
Snosey-Mac <ahmed.el.snosey@gmail.com>
2020-05-01 09:34:23 +0200

    dd

--- app/src/main/java/com/dinnova/sharedlibrary/ui/CustomSearchView.java
+++ app/src/main/java/com/dinnova/sharedlibrary/ui/CustomSearchView.java
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.widget.SearchView;
-import androidx.core.content.ContextCompat;
 
 import com.dinnova.sharedlibrary.R;
 
@@ -25,14 +24,11 @@ public class CustomSearchView extends SearchView {
 
     public CustomSearchView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        EditText searchEditText = findViewById(R.id.search_src_text);
-        searchEditText.setTextColor(getResources().getColor(R.color.white));
-        searchEditText.setHintTextColor(getResources().getColor(R.color.white));
-        customAddition = new CustomAddition(context, searchEditText, attrs, R.styleable.CustomSearchView, R.styleable.CustomSearchView_customText, R.styleable.CustomSearchView_customFont);
-        customAddition.setCustomHint(customAddition.getCustomStringById(R.styleable.CustomSearchView_customHint));
-        customAddition.setCustomColor(R.styleable.CustomSearchView_textColor, R.styleable.CustomSearchView_hintColor);
         ImageView searchIcon = findViewById(R.id.search_button);
         ImageView closeIcon = findViewById(R.id.search_close_btn);
+        EditText searchEditText = findViewById(R.id.search_src_text);
+        customAddition = new CustomAddition(context, searchEditText, attrs, R.styleable.CustomSearchView, R.styleable.CustomSearchView_customText, R.styleable.CustomSearchView_customFont);
+        customAddition.setCustomColor(R.styleable.CustomSearchView_textColor, R.styleable.CustomSearchView_hintColor);
         searchIcon.setColorFilter(customAddition.typedArray.getColor(R.styleable.CustomSearchView_searchColor,getContext().getResources().getColor(R.color.black_8am2)), android.graphics.PorterDuff.Mode.MULTIPLY);
         closeIcon.setColorFilter(customAddition.typedArray.getColor(R.styleable.CustomSearchView_searchColor,getContext().getResources().getColor(R.color.black_8am2)), android.graphics.PorterDuff.Mode.MULTIPLY);
 
