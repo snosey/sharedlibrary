f52fa11ee722d91be171a85536afa11849b21192
Snosey-Mac <ahmed.el.snosey@gmail.com>
2020-08-23 02:09:31 +0200

    update

--- app/src/main/java/com/dinnova/sharedlibrary/notification/NotificationModel.java
+++ app/src/main/java/com/dinnova/sharedlibrary/notification/NotificationModel.java
@@ -1,19 +1,17 @@
 package com.dinnova.sharedlibrary.notification;
 
-import com.dinnova.sharedlibrary.webservice.JsonConverter;
 import com.dinnova.sharedlibrary.webservice.JsonConverter2;
-import com.dinnova.sharedlibrary.webservice.WebService;
 import com.google.gson.annotations.Expose;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 public class NotificationModel extends JsonConverter2 {
 
-    public static class channelTypeEnum{
-        public static int normal =1;
-        public static int call =2;
+    public static class channelTypeEnum {
+        public static int normal = 1;
+        public static int call = 2;
     }
+
     public int channelType;
 
     @Expose
@@ -38,7 +36,12 @@ public class NotificationModel extends JsonConverter2 {
     public String Body;
 
     public void convertData() {
-        TargetIdInt = Integer.parseInt(TargetId);
+        try {
+            TargetIdInt = Integer.parseInt(TargetId);
+        } catch (Exception e) {
+            TargetIdInt = 0;
+            e.printStackTrace();
+        }
         try {
             NotificationTypeModel = (com.dinnova.sharedlibrary.notification.NotificationType) new NotificationType().jsonToModel(NotificationType);
         } catch (JSONException e) {
